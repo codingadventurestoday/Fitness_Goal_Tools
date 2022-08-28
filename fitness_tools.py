8a8dba653fec96a7de04b1f71e18ec2868c93d2e
codingadventurestoday <97482585+codingadventurestoday@users.noreply.github.com>
2022-08-28 14:39:07 -0700

    Update fitness_tools.py
    Created an outline for the goal function and determine_bmr function

--- fitness_tools.py
+++ fitness_tools.py
@@ -2,8 +2,46 @@
 1. A calculator that helps estimate your calorie needs to reach your weight goals.
 2. A calculator that helps determine the break down for each macro nutritient you should consume.
 """
+def goal():
+    goal_path = input('')
+    duration = input('')
+    try: 
+        goal_path = int(goal_path)
+    except: 
+        print('You have not entered a number for what your goal is. Please enter 1-3.')
+        goal()
+        break
+    if goal_path == 1: 
+            
+    elif goal_path == 2:
+    
+    elif goal_path == 3:
+        
+    else:
+        print('The number you entered is not an option. Please enter 1-3 base off of what you would like to accomplish.')
+        goal()
+        break 
 
-def weight_calculator: 
+def determine_bmr(): 
+    name = input('What is your name? : ')
+    #include a loop and regex to ensure gender is male or female, age, weight, height are numbers
+    gender = input(f'Hey {name}, we are going to need some information to help determine your calorie needs. What is your gender?: ')
+    age = input('Alright, now that we have your gender let\'s get your age. How old are you?: ')
+    weight = input('Next up. How much do you weight in Lbs?: ')
+    height = input('Last part before we give you your baseline caloric needs! How tall are you in inches?: ') 
+    
+    if gender.lower() == 'female':
+        BMR = (10*weight)+(6.25*height)-(5*age)-161
+        
+    elif gender.lower() == 'male':
+        BMR = (10*weight)+(6.25*height)-(5*age)+5
+    
+    goal():
+    
+    
+def weight_calculator(): 
+    determine_bmr()
+    goal()
     return None 
   
 def macro_calculator():
